22bd309b2f9d888d14f527f71ff5cc2fe2141337
BABUNI <46104458+sairbabu58@users.noreply.github.com>
2024-09-29 13:10:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
-<body style="background-color:red;">
+<body style="background-color:blue;">
 
-<h1>Hello J2C Technologies.....!!!!!!!!! </h1>
+<h1> Hello J2C Technologies.....!!!!!!!!! </h1>
 
 
 </body>
